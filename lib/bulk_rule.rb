23add103f622a561c557ae4703b6a9835abbed82
Andre Miglioranza <amilhoranza@gmail.com>
2019-03-27 12:42:43 -0300

    Modifying rule to set a new price for a product

--- lib/bulk_rule.rb
+++ lib/bulk_rule.rb
@@ -1,18 +1,25 @@
 require_relative 'base_rule'
-
+# BulkRule
 class BulkRule < BaseRule
-  def initialize(product_code:, quantity_eligible:, amount_discounted:)
-      @product_code = product_code
-      @quantity_eligible = quantity_eligible
-      @amount_discounted = amount_discounted
+  def initialize(product_code:, quantity_eligible:, discounted_price:)
+    @product_code = product_code
+    @quantity_eligible = quantity_eligible
+    @discounted_price = discounted_price
   end
 
   def calculate_discount(items)
-    local_items = items.select{|item| item.code == product_code}
+    local_items = items.select do |item|
+      item.code == product_code
+    end
+
     return total_items(items) if local_items.size < quantity_eligible
-    items.each { |item| item.price = item.price - amount_discounted if item.code == product_code  }
+
+    items.each do |item|
+      item.price = discounted_price if item.code == product_code
+    end
   end
 
   private
-  attr_reader :product_code, :quantity_eligible, :amount_discounted
+
+  attr_reader :product_code, :quantity_eligible, :discounted_price
 end
