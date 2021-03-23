358f53958b3840dcb1e9cd2ef438443f8da80479
Andre Miglioranza <amilhoranza@gmail.com>
2021-03-23 16:07:31 -0300

    notimplemented error

--- lib/base_rule.rb
+++ lib/base_rule.rb
@@ -1,5 +1,9 @@
 # BaseRule class
 class BaseRule
+  def calculate_discount(items)
+    raise NotImplementedError, 'Should be implemented in concrete class!'
+  end
+
   def total_items(items)
     items.map(&:price).reduce(0, :+)
   end
